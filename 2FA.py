a7216b2e2997286097486ce8313093df99aee18a
benginN <88718878+benginN@users.noreply.github.com>
2021-12-22 22:52:26 +0300

    Update 2FA.py

--- 2FA.py
+++ 2FA.py
@@ -1,4 +1,4 @@
-#Don't forget to import the things below and change 'GMAIL' and 'PASSWORD'as yours. The mail that logged in must have "Less Secure Apps" allowed.
+#Don't forget to import the things below and change 'GMAIFROM', GMA and 'PASSWORD'as yours. The mail that logged in must have "Less Secure Apps" allowed. 
 
 import random
 import requests
@@ -14,8 +14,8 @@ facode= (str(int(randomised)))
 
 
 msg = MIMEMultipart()
-msg['From'] = 'orhan123321bengin@gmail.com'
-msg['To'] = 'orhan123321bengin@gmail.com'
+msg['From'] = 'GMAILFROM'
+msg['To'] = 'GMAILTO'
 msg['Subject'] = 'Your Verification Code'
 message = "Your Verification Code Is: " + facode
 msg.attach(MIMEText(message))
@@ -24,12 +24,12 @@ mailserver = smtplib.SMTP('smtp.gmail.com',587)
 mailserver.ehlo()
 mailserver.starttls()
 mailserver.ehlo()
-mailserver.login('GMAIL', 'PASSWORD')
-mailserver.sendmail('orhan123321bengin@gmail.com','orhan123321bengin@gmail.com',msg.as_string())
+mailserver.login('GMAILFROM', 'PASSWORD')
+mailserver.sendmail('GMAILFROM','GMAILTO',msg.as_string())
 mailserver.quit()
 
 while True:
- facodetry = input("G-Mailinize Gelen 2 Faktörlü Doğrulama Kodunu Giriniz: ")
+ facodetry = input("Enter the Verification Code That Has Been Sent To Your Mailbox: ")
  if facodetry == facode:
     print ("You Have Succesfully Logged In.")
     break
